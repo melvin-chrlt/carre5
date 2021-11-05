7e2b26ac94c4902c2dbe12e95cd1bed165f81baf
Axel Gromat <axel.gromat@gmail.com>
2021-11-05 17:03:03 +0100

    interface + signup

--- app/sign-up.php
+++ app/sign-up.php
@@ -1,76 +1,63 @@
-<body class="g-sidenav-show  bg-gray-100" cz-shortcut-listen="true">
-  <!-- Navbar -->
-  
-  <!-- End Navbar -->
-  <section class="min-vh-100 mb-8">
-    <div class="page-header align-items-start min-vh-50 pt-5 pb-11 m-3 border-radius-lg" style="background-image: url('assets/img/curved-images/curved14.jpg');">
-      <span class="mask bg-gradient-dark opacity-6"></span>
-      <div class="container">
-        <div class="row justify-content-center">
-          <div class="col-lg-5 text-center mx-auto">
-            <h1 class="text-white mb-2 mt-5">Welcome!</h1>
-            <p class="text-lead text-white">Use these awesome forms to login or create new account in your project for free.</p>
-          </div>
+<?php
+include_once "_head.php";
+?>
+
+<section class="min-vh-100 mb-8">
+    <div class="page-header align-items-start min-vh-50 pt-5 pb-11 m-3 border-radius-lg"
+        style="background-image: url('assets/img/curved-images/curved14.jpg');">
+        <span class="mask bg-gradient-dark opacity-6"></span>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-5 text-center mx-auto">
+                    <h1 class="text-white mb-2 mt-5">Welcome!</h1>
+                    <p class="text-lead text-white">Use these awesome forms to login or create new account in your
+                        project for free.</p>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
     <div class="container">
-      <div class="row mt-lg-n10 mt-md-n11 mt-n10">
-        <div class="col-xl-4 col-lg-5 col-md-7 mx-auto">
-          <div class="card z-index-0">
-            <div class="card-header text-center pt-4">
-              <h5>Register with</h5>
+        <div class="row mt-lg-n10 mt-md-n11 mt-n10">
+            <div class="col-xl-4 col-lg-5 col-md-7 mx-auto">
+                <div class="card z-index-0">
+                    <div class="card-header text-center pt-4">
+                        <h5>Register with</h5>
+                    </div>
+
+                    <div class="card-body">
+                        <form role="form text-left">
+                            <div class="mb-3">
+                                <input type="text" class="form-control" placeholder="Name" aria-label="Name"
+                                    aria-describedby="email-addon">
+                            </div>
+                            <div class="mb-3">
+                                <input type="email" class="form-control" placeholder="Email" aria-label="Email"
+                                    aria-describedby="email-addon">
+                            </div>
+                            <div class="mb-3">
+                                <input type="password" class="form-control" placeholder="Password" aria-label="Password"
+                                    aria-describedby="password-addon">
+                            </div>
+                            <div class="form-check form-check-info text-left">
+                                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"
+                                    checked="">
+                                <label class="form-check-label" for="flexCheckDefault">
+                                    I agree the <a href="javascript:;" class="text-dark font-weight-bolder">Terms
+                                        and Conditions</a>
+                                </label>
+                            </div>
+                            <div class="text-center">
+                                <button type="button" class="btn bg-gradient-dark w-100 my-4 mb-2">Sign up</button>
+                            </div>
+                            <p class="text-sm mt-3 mb-0">Already have an account? <a href="javascript:;"
+                                    class="text-dark font-weight-bolder">Sign in</a></p>
+                        </form>
+                    </div>
+                </div>
             </div>
-            
-            <div class="card-body">
-              <form role="form text-left">
-                <div class="mb-3">
-                  <input type="text" class="form-control" placeholder="Name" aria-label="Name" aria-describedby="email-addon">
-                </div>
-                <div class="mb-3">
-                  <input type="email" class="form-control" placeholder="Email" aria-label="Email" aria-describedby="email-addon">
-                </div>
-                <div class="mb-3">
-                  <input type="password" class="form-control" placeholder="Password" aria-label="Password" aria-describedby="password-addon">
-                </div>
-                <div class="form-check form-check-info text-left">
-                  <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" checked="">
-                  <label class="form-check-label" for="flexCheckDefault">
-                    I agree the <a href="javascript:;" class="text-dark font-weight-bolder">Terms and Conditions</a>
-                  </label>
-                </div>
-                <div class="text-center">
-                  <button type="button" class="btn bg-gradient-dark w-100 my-4 mb-2">Sign up</button>
-                </div>
-                <p class="text-sm mt-3 mb-0">Already have an account? <a href="javascript:;" class="text-dark font-weight-bolder">Sign in</a></p>
-              </form>
-            </div>
-          </div>
         </div>
-      </div>
     </div>
-  </section>
-  
-  
-  
-  <!--   Core JS Files   -->
-  <script src="assets/js/core/popper.min.js"></script>
-  <script src="assets/js/core/bootstrap.min.js"></script>
-  <script src="assets/js/plugins/perfect-scrollbar.min.js"></script>
-  <script src="assets/js/plugins/smooth-scrollbar.min.js"></script>
-  <script>
-    var win = navigator.platform.indexOf('Win') > -1;
-    if (win && document.querySelector('#sidenav-scrollbar')) {
-      var options = {
-        damping: '0.5'
-      }
-      Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
-    }
-  </script>
-  <!-- Github buttons -->
-  <script async="" defer="" src="https://buttons.github.io/buttons.js"></script>
-  <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
-  <script src="assets/js/soft-ui-dashboard.min.js?v=1.0.3"></script>
-
-
-</body>
+</section>
+<?php
+include_once "_footer.php";
+?>
