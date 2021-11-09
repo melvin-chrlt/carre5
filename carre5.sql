1d006769b91a130d60c02ba2295bbcdd4ab37db6
Axel Gromat <axel.gromat@gmail.com>
2021-11-09 09:57:23 +0100

    fixed sql

--- carre5.sql
+++ carre5.sql
@@ -2,13 +2,13 @@ DROP DATABASE IF EXISTS carrefive;
 CREATE DATABASE carrefive CHARACTER SET utf8;
 USE carrefive;
 
-CREATE TABLE user (
+CREATE TABLE carrefive.user (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR (25) NOT NULL,
     password VARCHAR (512) NOT NULL
 );
 
-CREATE TABLE product (
+CREATE TABLE carrefive.product (
     product_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR (255) NOT NULL,
     description TEXT NOT NULL,
@@ -17,8 +17,8 @@ CREATE TABLE product (
     dlc DATE
 );
 
-INSERT INTO product (name,description,price,dlc) VALUES
-('PANZANI Coquillettes', 'Des coquillettes panzani !', 0.76, 2025-11-09),
-('MAGNUM Classic x4', 'Boite de 4 magnums classic', 2.34, 2022-05-09),
+INSERT INTO carrefive.product (name,description,price,dlc) VALUES
+('PANZANI Coquillettes', 'Des coquillettes panzani !', 0.76, '2025-11-09'),
+('MAGNUM Classic x4', 'Boite de 4 magnums classic', 2.34, '2022-05-09'),
 ('CARRE5 500 Feuilles A4', 'Rame de papier A4 90gr', 4.01, null),
 ('PAIC Citron', 'Du liquide vaisselle', 1.76, null);
